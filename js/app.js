70b77b5156e7292892759894536083060336e374
Jared Smith <jaredsmith07@gmail.com>
2018-11-11 01:02:11 -0600

    branch starting files from force rank repo

--- js/app.js
+++ js/app.js
@@ -1,24 +1,7 @@
 // Start Program
 // ================================================================================
-createListItem("Chocolate");
-createListItem("Vanilla");
-createListItem("Strawberry");
-createListItem("Mint Chocolate Chip");
-createListItem("Cookies n' Creme");
-
-outputList();
 
 
 
 // For Dev
 // ================================================================================
-// document.addEventListener("click", (event) => {
-//     log(event.target);
-// });
-
-// log list button
-const logListButton = document.querySelector('#loglist');
-logListButton.addEventListener("click", () => {
-    log(list);
-});
-
